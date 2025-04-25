085a95a594a12b6ac279563b9a5bed2db5ab4f5d
jimenaamr <202105740@alu.comillas.edu>
2025-04-25 17:31:18 +0200

    Cambio 4

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ def http_hello_world(request: Request):
     request_json = request.get_json(silent=True)
     name = request_json.get('name', 'World') if request_json else 'World'
     
-    return f"Helloo, {name} from Cloud Functions!", 200
+    return f"Hello, {name} from Cloud Functions!", 200
 
